744cd4a17c6c8b2df9804f1e8e72d33d0fd25281
Fabiano Pellenz <fabiano@ekonovaadv.com.br>
2026-07-24 18:12:05 -0300

    Use white Azimut logo on dark backgrounds in app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { FormEvent, useEffect, useMemo, useState } from "react";
 import { getSavedLanguage, saveLanguage, SiteLanguage } from "./site-language";
 
 const logo = "/tr3-logo-new.svg";
-const azimutMenuLogo = "/azimut-extremo-logo.png";
+const azimutMenuLogo = "/azimut-extremo-logo-white.svg";
 const legendsLogo = "https://www.legendsbikerace.com.br/legends-logo-official.png";
 
 const homeNavigation = {
