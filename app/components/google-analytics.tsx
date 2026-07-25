f489570a6c9317bfa0b2f010ec9bf2090f7130ef
Fabiano Pellenz <fabiano@ekonovaadv.com.br>
2026-07-24 21:38:57 -0300

    Connect Google Analytics tracking

---
+++ app/components/google-analytics.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import Script from "next/script";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const GOOGLE_ANALYTICS_ID = "G-HD7JY3MSNR";
+
+type GtagWindow = Window & {
+  gtag?: (...args: unknown[]) => void;
+};
+
+export function GoogleAnalytics() {
+  const pathname = usePathname();
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    if (!isReady || !pathname) {
+      return;
+    }
+
+    const gtag = (window as GtagWindow).gtag;
+
+    if (!gtag) {
+      return;
+    }
+
+    gtag("event", "page_view", {
+      page_location: window.location.href,
+      page_path: pathname,
+      page_title: document.title,
+    });
+  }, [isReady, pathname]);
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
+        strategy="afterInteractive"
+        onLoad={() => setIsReady(true)}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          window.gtag = gtag;
+          gtag('js', new Date());
+          gtag('config', '${GOOGLE_ANALYTICS_ID}', {
+            send_page_view: false
+          });
+        `}
+      </Script>
+    </>
+  );
+}
