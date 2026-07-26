510959811997d0b63633e528484c962f40721b49
Fabiano Pellenz <fabiano@ekonovaadv.com.br>
2026-07-26 13:09:51 -0300

    Separa Analytics da Threerace e Legends

--- app/components/google-analytics.tsx
+++ app/components/google-analytics.tsx
@@ -5,7 +5,7 @@ import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
 // Google tag assigned by GA4 to the Threerace production web stream.
-const GOOGLE_ANALYTICS_ID = "G-HD7JY3MSNR";
+const GOOGLE_ANALYTICS_ID = "G-6DQQNX5CRQ";
 
 type GtagWindow = Window & {
   gtag?: (...args: unknown[]) => void;
